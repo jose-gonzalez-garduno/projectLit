557d8809275e4222573682a5f04b6d1fc1b2a9b5
Jacob Burton <session1@START-Coding-MBP-03.local>
2017-02-23 15:15:02 -0700

    we did it?

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,6 +2,10 @@ var artistControllers = angular.module('artistControllers', [
   'ngAnimate'
 ]);
 
+artistControllers.factory('speakers', function($http) {
+    return $http.get('js/data.json');
+});
+
 artistControllers.controller('IndexController', ['$scope', '$http', function($scope, $http) {
    $http.get('js/data.json')
        .then(function(res) {
@@ -17,6 +21,7 @@ artistControllers.controller('SpeakersController', ['$scope', '$http', function(
        .then(function(res) {
            $scope.speakers = res.data;
            $scope.page = 'artistList';
+           $scope.summary = true;
        }, function(err) {
            console.log('HTTP request failed with result: ' + err);
        });
