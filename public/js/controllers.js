2072cb6f0b2b97af65255fca138b341f7ebc3e4b
Jacob Burton <session1@START-Coding-MBP-03.local>
2017-02-23 14:08:10 -0700

    2-23 commit

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,10 +2,22 @@ var artistControllers = angular.module('artistControllers', [
   'ngAnimate'
 ]);
 
-artistControllers.controller('IndexController', function($scope) {
-    $scope.page = 'Home';
-});
+artistControllers.controller('IndexController', ['$scope', '$http', function($scope, $http) {
+   $http.get('js/data.json')
+       .then(function(res) {
+           $scope.speakers = res.data;
+           $scope.page = 'home';
+       }, function(err) {
+           console.log('HTTP request failed with result: ' + err);
+       });
+}]);
 
-artistControllers.controller('SpeakersController', ['$scope', function($scope) {
-  $scope.page = 'artistsList';
+artistControllers.controller('SpeakersController', ['$scope', '$http', function($scope, $http) {
+   $http.get('js/data.json')
+       .then(function(res) {
+           $scope.speakers = res.data;
+           $scope.page = 'artistList';
+       }, function(err) {
+           console.log('HTTP request failed with result: ' + err);
+       });
 }]);
